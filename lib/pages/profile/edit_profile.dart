6c44f60f229aada618e84e3e10523d53d8d6630e
hetalporiya <byttezhetal@gmail.com>
2024-08-05 19:04:52 +0530

    create dairy images,date of birth screen,edit profile issue solve(05-08-2024)

--- lib/pages/profile/edit_profile.dart
+++ lib/pages/profile/edit_profile.dart
@@ -17,112 +17,114 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+
+  final nameCon = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-            backgroundColor: ThemeSetting.of(context).secondaryBackground,
-            appBar: HeaderWidget.headerWithTitle(
-        context: context, title: LocaleKeys.edit_profile.tr()),
-            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-
-            floatingActionButton:    Padding(padding: EdgeInsets.symmetric(vertical: 10,horizontal: 18),child: ButtonWidget.roundedButtonOrange(
-                context: context,
-                width: MediaQuery.of(context).size.width,
-                color: ThemeSetting.of(context).black2,
-                text: LocaleKeys.save.tr(),
-                onTap: () {}),),
-            body: ListView(
-      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
-      children: [
-        Stack(
-          alignment: Alignment.center,
+        backgroundColor: ThemeSetting.of(context).secondaryBackground,
+        appBar: HeaderWidget.headerWithTitle(
+            context: context, title: LocaleKeys.edit_profile.tr()),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 18),
+          child: ButtonWidget.roundedButtonOrange(
+              context: context,
+              width: MediaQuery.of(context).size.width,
+              color: ThemeSetting.of(context).black2,
+              text: LocaleKeys.save.tr(),
+              onTap: () {}),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
           children: [
-            Image.asset(
-              width: 100,
-              height: 100,
-              AppAssets.user,
-              fit: BoxFit.cover,
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  width: 100,
+                  height: 100,
+                  AppAssets.user,
+                  fit: BoxFit.cover,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Image.asset(
+                    AppAssets.camera,
+                    width: 30,
+                    height: 30,
+                  ),
+                ),
+              ],
             ),
-            InkWell(
-              onTap: () {},
-              child: Image.asset(
-                AppAssets.camera,
-                width: 30,
-                height: 30,
-              ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              LocaleKeys.name.tr(),
+              style: ThemeSetting.of(context)
+                  .captionLarge
+                  .copyWith(color: ThemeSetting.of(context).primary),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomTextField(
+              controller: nameCon,
+              hintText: LocaleKeys.enter_your_name.tr(),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              LocaleKeys.gender.tr(),
+              style: ThemeSetting.of(context)
+                  .captionLarge
+                  .copyWith(color: ThemeSetting.of(context).primary),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomGenderToggleButton(
+              onGenderSelected: (p0) {},
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              LocaleKeys.date_of_birth.tr(),
+              style: ThemeSetting.of(context)
+                  .captionLarge
+                  .copyWith(color: ThemeSetting.of(context).primary),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomTextField(
+              controller: TextEditingController(),
+              hintText: 'January 1st, 1990',
+              onTap: () async {
+                context.pushNamed(dateOfBirthScreen);
+                // final DateTime? pickedDate = await showCustomDatePicker(
+                //   context: context,
+                //   initialDate: DateTime.now(),
+                //   firstDate: DateTime(1900),
+                //   lastDate: DateTime.now(),
+                // );
+                // if (pickedDate != null) {
+                //   print(pickedDate);
+                // }
+              },
+              readOnly: true,
+              suffix: const Icon(Icons.keyboard_arrow_right),
+            ),
+            const SizedBox(
+              height: 70,
             ),
           ],
         ),
-        const SizedBox(
-          height: 30,
-        ),
-        Text(
-          LocaleKeys.name.tr(),
-          style: ThemeSetting.of(context)
-              .captionLarge
-              .copyWith(color: ThemeSetting.of(context).primary),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        CustomTextField(
-          controller: TextEditingController(),
-          hintText: LocaleKeys.enter_your_name.tr(),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        Text(
-          LocaleKeys.gender.tr(),
-          style: ThemeSetting.of(context)
-              .captionLarge
-              .copyWith(color: ThemeSetting.of(context).primary),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        CustomGenderToggleButton(
-          onGenderSelected: (p0) {},
-        ),
-        const SizedBox(
-          height: 40,
-        ),
-        Text(
-          LocaleKeys.date_of_birth.tr(),
-          style: ThemeSetting.of(context)
-              .captionLarge
-              .copyWith(color: ThemeSetting.of(context).primary),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        CustomTextField(
-          controller: TextEditingController(),
-          hintText: 'January 1st, 1990',
-          onTap: () async {
-            context.pushNamed(dateOfBirthScreen);
-            // final DateTime? pickedDate = await showCustomDatePicker(
-            //   context: context,
-            //   initialDate: DateTime.now(),
-            //   firstDate: DateTime(1900),
-            //   lastDate: DateTime.now(),
-            // );
-            // if (pickedDate != null) {
-            //   print(pickedDate);
-            // }
-          },
-          readOnly: true,
-          suffix: const Icon(Icons.keyboard_arrow_right),
-        ),
-        const SizedBox(
-          height: 70,
-        ),
-
-
-      ],
-            ),
-          ),
+      ),
     );
   }
 }
